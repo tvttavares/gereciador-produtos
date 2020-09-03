0a6ca6b56d66f78450e23391368a7b29ab9d4165
Thiago Tavares <tvttavares@gmail.com>
2020-09-03 17:38:11 -0400

    Alteracao do titulo conforme navegacao nas paginas

--- gerenciador-produtos-ui/src/app/view/produtos-crud/produtos-crud.component.ts
+++ gerenciador-produtos-ui/src/app/view/produtos-crud/produtos-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-produtos-crud',
@@ -8,8 +9,13 @@ import { Router } from '@angular/router';
 })
 export class ProdutosCrudComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/produtos'
+    }
+  }
   ngOnInit(): void {
   }
 
